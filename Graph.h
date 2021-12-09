2621f1e2b61c2c68e5d3fd7a24afaca634efb93f
Liam Sy <79372899+liamrs222@users.noreply.github.com>
2021-12-09 18:01:08 -0500

    added findMostSimilarBusiness functions

--- Graph.h
+++ Graph.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <algorithm>
 #include "Business.h"
 using namespace std;
 
 class Graph {
+public:
     struct Vertex {
         Business* data;
         vector<Vertex*> adjVertices;
@@ -13,26 +15,36 @@ class Graph {
             this->adjVertices = adjVertices;
 
             //read the categories
-            istringstream stream(data->getCategories());
-            string temp;
-            while (getline(stream, temp, ','))
+		    istringstream stream(data->getCategories());
+		    string temp;
+            if (data->getCategories().find("Restaurants") != string::npos)
             {
-                if (temp.at(0) == ' ')
-                    temp = temp.substr(1, temp.length() - 1);
-                this->categories.insert(temp);
+                while (getline(stream, temp, ','))
+                {
+                    if (temp.at(0) == ' ')
+                        temp = temp.substr(1, temp.length() - 1);
+                    this->categories.insert(temp);
+                }
             }
         }
     };
 
     vector<Vertex*> adjList;
-    set<string> states;
-
+    unordered_map<string, vector<Vertex*>> states;
+    set<string> allCategories;
+	
 public:
-    Graph(vector<Business*>& businesses);
-    void Print();
-
+	Graph(vector<Business*>& businesses);
+    set<string> getStates();
+    set<string> getAllCategories();
+	void PrintStates();
+    void PrintCategories();
+    void PrintAdjacent();
     void bfs();
     void bfsState(string state);
     void dfs();
     void dfsState(string state);
+    vector<Business*> findMostSimilarBusiness(set<string>& userCategories);
+    vector<Business*> findMostSimilarBusiness(set<string>& userCategories, string state);
+    int countIntersection(set<string> a, set<string> b);
 };
